7ed207bd3c50f1833c960c5c98b115f225c32227
lieu1489 <lieuutehy21@gmail.com>
2020-10-17 02:09:01 +0700

    update by lieu10117330

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Bai1Component } from './bai1/bai1.component';
+import { Bai2Component } from './bai2/bai2.component';
+import { SearchComponent } from './bai2/search/search.component';
+import { TableModule } from 'primeng/table';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Bai1Component,
+    Bai2Component,
+    SearchComponent
   ],
   imports: [
+    FormsModule,
+    SharedModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    TableModule,
+    AccordionModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
